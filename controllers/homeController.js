6b6cf55060946c8be7e83fc3292ab9da0d693178
Roman Melnikov <78842633+RomanVaKo@users.noreply.github.com>
2023-03-12 23:36:08 +0300

    2

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,19 +1,21 @@
-const { parseArgs } = require("util")
 const FileService = require("../services/fileService")
 
+//возвращает страницу
 exports.index = function (request, response) {
-
     response.sendFile('index.html', { root: __dirname + '/../views' })
 }
 
+//запрос на получение данных из xlsx-файла
 exports.getAll = function (request, response) {
     const fileService = new FileService()
     const data = fileService.getAll()
     response.json(data)
 }
 
+//запрос на поиск данных в xlsx-файле
 exports.findItem = function (request, response) {
 
+    //параметр, получаемый во время запроса
     let name = request.query.value
 
     const fileService = new FileService()
